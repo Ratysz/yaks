caaf6f5891a4e71d1e005b3823a4dd0b225f1b11
Ratys <alexander.sepity@gmail.com>
2020-06-02 19:38:06 +0300

    Moved `QueryMarker` to it's own file, added note on improper use.

--- src/query_bundle.rs
+++ src/query_bundle.rs
@@ -1,90 +1,14 @@
 use hecs::{Component, Query, With, Without};
-use std::marker::PhantomData;
 
 #[cfg(feature = "parallel")]
 use hecs::World;
 #[cfg(feature = "parallel")]
 use std::any::TypeId;
 
+use crate::QueryMarker;
+
 #[cfg(feature = "parallel")]
 use crate::{ArchetypeSet, ComponentTypeSet};
-
-/// A zero-sized type used to describe the queries a system may require, as well as
-/// performing said queries within the system via methods of
-/// [`SystemContext`](struct.SystemContext.html).
-///
-/// Instantiating these directly is only useful when calling systems as plain functions,
-/// and can be done either by `QueryMarker::new()`, `QueryMarker::default()`, or
-/// `Default::default()`, the latter of which can also instantiate tuples of markers (up to 10):
-/// ```rust
-/// # use yaks::{SystemContext, QueryMarker};
-/// # let world = hecs::World::new();
-/// # let world = &world;
-/// # let mut average = 0f32;
-/// fn single_query(context: SystemContext, average: &mut f32, query: QueryMarker<&f32>) {
-///     *average = 0f32;
-///     let mut entities = 0;
-///     for (_entity, value) in context.query(query).iter() {
-///         entities += 1;
-///         *average += *value;
-///     }
-///     *average /= entities as f32;
-/// }
-/// single_query(world.into(), &mut average, QueryMarker::new());
-/// single_query(world.into(), &mut average, QueryMarker::default());
-///
-/// fn two_queries(
-///     context: SystemContext,
-///     average: &mut f32,
-///     (floats, ints): (QueryMarker<&f32>, QueryMarker<&i32>),
-/// ) {
-///     *average = 0f32;
-///     let mut entities = 0;
-///     for (_entity, value) in context.query(floats).iter() {
-///         entities += 1;
-///         *average += *value;
-///     }
-///     for (_entity, value) in context.query(ints).iter() {
-///         entities += 1;
-///         *average += *value as f32;
-///     }
-///     *average /= entities as f32;
-/// }
-/// two_queries(world.into(), &mut average, Default::default());
-/// ```
-pub struct QueryMarker<Q0>(PhantomData<Q0>)
-where
-    Q0: Query;
-
-impl<Q0> QueryMarker<Q0>
-where
-    Q0: Query,
-{
-    /// Equivalent to `QueryMarker::default()`. See documentation for `QueryMarker` itself.
-    pub fn new() -> Self {
-        Self(PhantomData)
-    }
-}
-
-impl<Q0> Clone for QueryMarker<Q0>
-where
-    Q0: Query,
-{
-    fn clone(&self) -> Self {
-        QueryMarker::new()
-    }
-}
-
-impl<Q0> Copy for QueryMarker<Q0> where Q0: Query {}
-
-impl<Q0> Default for QueryMarker<Q0>
-where
-    Q0: Query,
-{
-    fn default() -> Self {
-        Self::new()
-    }
-}
 
 pub trait QueryExt: Query {
     #[cfg(feature = "parallel")]
